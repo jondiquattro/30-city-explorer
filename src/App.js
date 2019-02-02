343a30556981baf36e1bcd01c4b6d347e5dbe9d4
Jonathan DiQuattro <jondiquattro@hotmail.com>
2019-02-02 12:37:21 -0800

    cleaned up

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React, { Component } from 'react';
 import superagent from 'superagent';
 // import './styles.scss';
 
+
+
+
 class Form extends React.Component {
   constructor(props) {
     super(props);
@@ -41,38 +44,21 @@ class Form extends React.Component {
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      count: 0,
-      people: [],
-      selectedPerson: '',
-    };
+    this.state = {};
+    //to update the state
+    
   }
 
-  handleForm = (count, people) => {
-    this.setState({ count, people });
-  };
-
-  showPerson = person => {
-    this.setState({ selectedPerson: person });
-  };
+  updateLocation = (location)=>{
+    this.setState({location: location});
+  }
 
   render() {
     return (
       <>
         <Form prompt="Update Location" handler={this.handleLocation} />
-        <p>{this.state.location}</p>
-        <h3>Selected Character: {this.state.selectedPerson}</h3>
-        <ul>
-          {Object.keys(this.state.people).map((person, idx) => {
-            return (
-              <li key={idx}>
-                <a href="#" onClick={() => this.showPerson(person)}>
-                  {person}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
+        <p> location is: {this.state.location} here</p>
+
       </>
     );
   }
